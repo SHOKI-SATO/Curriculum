4a163fcb43b9eec649cde621d332d0d74b5b3e41
shoki_sato <ringhio324@icloud.com>
2021-09-03 23:20:33 +0900

    DOM
    2-1

---
+++ 2-1/js/main.js
@@ -0,0 +1,10 @@
+//windowオブジェクトの中身をconsole.logで確認してみます。
+console.log(window);
+
+//①location.hrefでURLを取得して、変数urlに代入。
+let url = window.location.href;
+
+//②ブラウザを更新すると、URLが表示されます。
+console.log(url);
+
+window.alert('Hello World');
